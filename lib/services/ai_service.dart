7dd60966aa52c7899e8a1024bdb5b117b5408d54
damlakaragoz <phoebesblogspot@gmail.com>
2024-11-01 02:47:07 +0300

    ai integration to workspace Changes to be committed: 	modified:   lib/services/ai_service.dart

--- lib/services/ai_service.dart
+++ lib/services/ai_service.dart
@@ -14,7 +14,7 @@ class AiService {
   }
 
   final UserDbService _userDbService;
-  final String _apiKey = "AIzaSyDPNPq14ivElRmmjmlQsBGW21X2WSFGGJ8";
+  final String _apiKey = "AIzaSyDPNPq14ivElRmmjmlQsBGW21X2WSFGGJ8"; //Gemini API
   
   late String userAge;
   late String userOccupation;
